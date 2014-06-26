725aaa4a27abe79d73fa32b83f7f1d63c1fc4914
Jan Marsch <jama@keks.com>
2014-06-26 15:16:54 +0200

    prepared missing tests

---
+++ test/geojson.spec.js
@@ -0,0 +1,28 @@
+
+module('geojson');
+
+QUnit.testStart(function() {});
+
+/***
+test('Should load GeoJSON and return formatted objects', function() {
+
+  VECNIK.GeoJSON.load = function(url, tileCoords, projection, callback) {
+//  if (!VECNIK.GeoJSON.WEBWORKERS || typeof Worker === undefined) {
+
+    if (typeof Worker === undefined) {
+      VECNIK.load(url, function(collection) {
+        callback(_convertAndReproject(collection, projection, tileCoords));
+      });
+    } else {
+      var worker = new Worker('../src/reader/geojson.worker.js');
+    }
+
+//  cartodbApi.load({ x:x, y:y, z:z }, callback);
+//  // callback is called
+  };
+
+
+
+});
+
+***/
