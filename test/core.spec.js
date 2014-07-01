9190db8c0e6041b61b103b0aff22dd20a66c67d5
javi <qualopec@gmail.com>
2014-07-01 15:20:05 +0200

    fixed asyn test

--- test/core.spec.js
+++ test/core.spec.js
@@ -9,15 +9,8 @@ test('Geometry types need to be defined in VECNIK.Geometry.*', function() {
   equal(VECNIK.Geometry.POLYGON, 'Polygon');
 });
 
-/*
-asyncTest('XHR', function(assert) {
-      assert.ok(true, 'video has loaded and is ready to play');
-    QUnit.start();
-
+asyncTest('XHR', 1, function() {
   VECNIK.load('test.json', function(res) {
-
-//    equal(res, {test:123});
-//    console.log(res)
+    equal(res, {test:123});
   });
 });
-*/
