105eee058df57f756c59b61bf68021ac42abe106
Jan Marsch <jama@keks.com>
2014-07-17 13:25:02 +0200

    test fix for hitshader

--- test/cartoshader.spec.js
+++ test/cartoshader.spec.js
@@ -4,27 +4,29 @@ module('cartoshader');
 var shader;
 
 QUnit.testStart(function(details) {
-  shader = new VECNIK.CartoShader(JSON.stringify({
-    'point-color': '#ffffff',
-    'line-color': function(data) {
-      return data.color;
-    },
-    'line-width': 1,
-    'polygon-fill': '#0000ff'
-  }));
-
-console.log('SHADER', shader)
+  shader = new VECNIK.CartoShader(
+    '#layer {\n'+
+    '  polygon-fill: rgba(0, 0, 0, 0.3);\n'+
+    '  line-color: white;\n'+
+    '  line-width: 4;\n'+
+    '  [numfloors > 10] {\n'+
+    '    polygon-fill: rgba(255, 0, 0, 0.7);\n'+
+    '  }\n'+
+    '}'
+  );
 });
 
 test('create an interactivity shader', function() {
-  var hit = shader.createHitShader('id');
+  var hitShader = shader.createHitShader('id');
+  var hitLayer = hitShader.getLayers()[0];
+  var style;
 
-  var style = hit.getStyle({ id: 0 });
+  style = hitLayer.getStyle({ id: 0 });
   equal(style.polygonFill, 'rgb(1,0,0)');
 
-  style = hit.getStyle({ id: 1 });
+  style = hitLayer.getStyle({ id: 1 });
   equal(style.polygonFill, 'rgb(2,0,0)');
 
-  style = hit.getStyle({ id: 256 });
+  style = hitLayer.getStyle({ id: 256 });
   equal(style.polygonFill, 'rgb(1,1,0)');
 });
