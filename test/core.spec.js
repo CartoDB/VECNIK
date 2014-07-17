b46dad278fe19ac736ea5a5512c01c4eaec099e4
Jan Marsch <jama@keks.com>
2014-07-17 23:03:02 +0200

    re-enabling xhr test

--- test/core.spec.js
+++ test/core.spec.js
@@ -5,14 +5,13 @@ module('core');
 
   QUnit.testStart(function() {});
 
-// doesn't work from command line but browser is fine
-
-//  asyncTest('XHR', function() {
-//    var expectedRes = JSON.stringify({ test: 123 });
-//    VECNIK.load('test.json', function(res) {
-//      equal(JSON.stringify(res), expectedRes);
-//      start();
-//    });
-//  });
+  // doesn't work from command line but browser is fine
+  asyncTest('XHR', function() {
+    var expectedRes = JSON.stringify({ test: 123 });
+    VECNIK.load('test.json', function(res) {
+      equal(JSON.stringify(res), expectedRes);
+      start();
+    });
+  });
 
 }());
