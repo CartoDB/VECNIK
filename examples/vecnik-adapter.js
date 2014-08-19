7153187f636edce44bbb99bbeca41cc06c115a42
Jan Marsch <jama@keks.com>
2014-08-19 10:35:12 +0200

    merge

--- examples/vecnik-adapter.js
+++ examples/vecnik-adapter.js
@@ -36,7 +36,7 @@ function addTileLayer(options) {
   });
 }
 
-function addVecnikLayer(options, columns) {
+function addVecnikLayer(options) {
   var layerOptions = options.layer_definition.layers[0].options
   var sql = layerOptions.sql;
   var table = sql.replace(/^.+\sFROM\s+/i, '');
@@ -46,13 +46,61 @@ console.log(table);
   var provider = new VECNIK.CartoDB.API(VECNIK.GeoJSON, {
     user: options.user_name,
     table: table,
-    columns: columns,
+    columns: ['*'],
     debug: false,
     bufferSize: 50
   });
 
-console.log(layerOptions.cartocss);
-  var shader = new VECNIK.CartoShader(layerOptions.cartocss);
+  var cartocss = layerOptions.cartocss;
+/***
+
+cartocss = '#layer { line-color:#ff0000;line-width:5; }';
+
+cartocss =
+'#network[zoom>=5][zoom<=8] {\
+  line-color: rgba(0, 0, 0, 0);\
+  [highway=\'"motorway"\'] { line-color: #000; }\
+  [highway=\'"trunk"\'] { line-color: #300; }\
+  [zoom=5] {\
+    [highway=\'"motorway"\'] { line-width: 0.4; }\
+    [highway=\'"trunk"\'] { line-width: 0.2; } }\
+  [zoom=6] {\
+    [highway=\'"motorway"\'] { line-width: 0.5; }\
+    [highway=\'"trunk"\'] { line-width: 0.25; } }\
+  [zoom=7] {\
+    [highway=\'"motorway"\'] { line-width: 0.6; }\
+    [highway=\'"trunk"\'] { line-width: 0.3; } }\
+  [zoom=8] {\
+    [highway=\'"motorway"\'] { line-width: 1; }\
+    [highway=\'"trunk"\'] { line-width: 0.5; } }\
+}\
+\
+#network[zoom>=9][zoom<=14] {\
+  line-color: rgba(0, 0, 0, 0);\
+  [highway=\'"motorway"\'], [highway=\'"motorway_link"\'] {\
+    line-color: #000;\
+  }\
+  [highway=\'"trunk"\'], [highway=\'"trunk_link"\'] {\
+    line-color: #300;\
+  }\
+  [highway=\'"primary"\'] { line-color: #000; }\
+  [highway=\'"secondary"\'] { line-color: #000; }\
+  [highway=\'"tertiary"\'] { line-color: #C00; }\
+  [zoom=9] {\
+    [highway=\'"motorway"\'],[highway=\'"trunk"\'] { line-width: 1.4; }\
+    [highway=\'"primary"\'],[highway=\'"secondary"\'],\
+    [highway=\'"motorway_link"\'],[highway=\'"trunk_link"\'] { line-width: 0.6; }\
+  }\
+  [zoom=10] {\
+    [highway=\'"motorway"\'],[highway=\'"trunk"\'] { line-width: 1.8; }\
+    [highway=\'"primary"\'],[highway=\'"secondary"\'],\
+    [highway=\'"motorway_link"\'],[highway=\'"trunk_link"\'] { line-width: 0.8; }\
+  }\
+}';
+
+***/
+console.log(cartocss);
+  var shader = new VECNIK.CartoShader(cartocss);
 
   var layer = new VECNIK.Layer({
     provider: provider,
@@ -66,9 +114,9 @@ console.log(layerOptions.cartocss);
     for (var key in e.feature) {
       innerHTML += '<div class="property"><span class="key">'+ key +'</span>'+ e.feature[key] +'</div>';
     }
-    tooltip.innerHTML     = innerHTML;
-    tooltip.style.left    = (e.x+10) +'px';
-    tooltip.style.top     = (e.y+10) +'px';
+    tooltip.innerHTML  = innerHTML;
+    tooltip.style.left = (e.x+10) +'px';
+    tooltip.style.top  = (e.y+10) +'px';
   })
   .on('featureOver', function(e) {
     tooltip.style.left = (e.x+10) +'px';
@@ -81,7 +129,7 @@ console.log(layerOptions.cartocss);
   return layer;
 }
 
-function initVecnikAdapter(containerId, vizUrl, refMap, columns) {
+function initVecnikAdapter(containerId, vizUrl, refMap) {
   var map = new L.Map(containerId);
 
   JSONP(vizUrl +'?callback', function(viz) {
@@ -94,24 +142,35 @@ function initVecnikAdapter(containerId, vizUrl, refMap, columns) {
     var layer;
     for (var i = 0; i < viz.layers.length; i++) {
       layer = viz.layers[i];
-
 //      if (layer.type === 'background') {
 //        addBackgroundLayer(layer.options).addTo(map);
 //      }
+
+      // exclusive replacement for gmaps layer in 'network' example
+      if (layer.type === 'gmapsbase') {
+        new L.TileLayer(
+          'http://{s}.tile.stamen.com/toner/{z}/{x}/{y}.png',
+          {
+            subdomains: 'abc',
+            attribution: '&copy; Stamen Toner',
+            maxZoom: 18
+          })
+          .addTo(map);
+      }
 
       if (layer.type === 'tiled') {
         addTileLayer(layer.options).addTo(map);
       }
 
       if (layer.type === 'layergroup') {
-        addVecnikLayer(layer.options, columns).addTo(map);
+        addVecnikLayer(layer.options).addTo(map);
       }
     }
 
     setTimeout(function() {
       map.setView(refMap.get('center'), refMap.get('zoom'));
     }, 250);
-        
+
     refMap.on('change:zoom change:center', function(e) {
       map.setView(refMap.get('center'), refMap.get('zoom'));
     });
@@ -123,6 +182,6 @@ function initVecnikAdapter(containerId, vizUrl, refMap, columns) {
 //      });
 //    });
   });
-  
+
   return map;
 }
